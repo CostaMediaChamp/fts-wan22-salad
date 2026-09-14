571b26dc057e689755114ee09b6d68d9a7e2a702
CostaMediaChamp <costapassa1969@gmail.com>
2026-09-14 14:29:56 +0300

    Add startup diagnostic timestamps

--- start-v03b.sh
+++ start-v03b.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-cd /opt
+echo "[FTST-DIAG] $(date -u '+%Y-%m-%dT%H:%M:%S.%3NZ') ENTRYPOINT iniziato"
 
+cd /opt
+echo "[FTST-DIAG] $(date -u '+%Y-%m-%dT%H:%M:%S.%3NZ') Avvio Job Queue Worker"
 ./salad-http-job-queue-worker &
 
-echo "Starting ComfyUI API..."
+echo "[FTST-DIAG] $(date -u '+%Y-%m-%dT%H:%M:%S.%3NZ') Avvio comfyui-api"
 exec /opt/ComfyUI/comfyui-api
